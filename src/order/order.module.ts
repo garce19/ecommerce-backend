035ecbaf2441692ddf684d67ccfd4d362416c949
Germán Rojas <garcejr@gmail.com>
2025-06-07 17:35:43 -0500

    Add OrderModule and OrderService with TypeORM integration for OrderEntity

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderEntity } from './order.entity/order.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([OrderEntity])],
+  providers: [OrderService]
+})
 export class OrderModule {}
