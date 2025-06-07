311d4a9efb8397727f1bac3cc7d814b7e24f59a1
Germán Rojas <garcejr@gmail.com>
2025-06-07 18:19:09 -0500

    Implement ProductService with CRUD operations and error handling

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './product.entity/product.entity';
 import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from 'src/shared/business-errors';
 
 @Injectable()
 export class ProductService {
@@ -9,4 +10,48 @@ export class ProductService {
         @InjectRepository(ProductEntity)
         private readonly productRepository: Repository<ProductEntity>
     ) { }
+
+    async findAll(): Promise<ProductEntity[]> {
+        return this.productRepository.find({ relations: ['seller', 'orders', 'shoppingBags'] });
+    }
+
+    async findOne(id: string): Promise<ProductEntity> {
+        const product = await this.productRepository.findOne({ where: { id }, relations: ['seller', 'orders', 'shoppingBags'] });
+        if (!product) {
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+        }
+        return product;
+    }
+
+    async create(product: ProductEntity): Promise<ProductEntity> {
+        if (!product.seller) {
+            throw new BusinessLogicException("The product must have a seller", BusinessError.PRECONDITION_FAILED);
+        }
+
+        if (product.unitPrice < 0) {
+            throw new BusinessLogicException("The product price cannot be negative", BusinessError.PRECONDITION_FAILED);
+        }
+
+        if (product.quantity < 0) {
+            throw new BusinessLogicException("The product quantity cannot be negative", BusinessError.PRECONDITION_FAILED);
+        }
+
+        return this.productRepository.save(product);
+    }
+
+    async update(id: string, product: ProductEntity): Promise<ProductEntity> {
+        const persistedProduct = await this.productRepository.findOne({ where: { id } });
+        if (!persistedProduct) {
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+        }
+        return this.productRepository.save({ ...persistedProduct, ...product });
+    }
+
+    async delete(id: string) {
+        const product = await this.productRepository.findOne({ where: { id } });
+        if (!product) {
+            throw new BusinessLogicException("The product with the given id was not found", BusinessError.NOT_FOUND);
+        }
+        await this.productRepository.remove(product);
+    }
 }
