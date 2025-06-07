b8a700b11949dcb2009e8fff69a2ba66dff2eb3d
Germán Rojas <garcejr@gmail.com>
2025-06-07 17:22:39 -0500

    Add TypeORM configuration to AppModule for PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { UserModule } from './user/user.module';
 import { OrderModule } from './order/order.module';
 import { ProductModule } from './product/product.module';
 import { ShoppingBagModule } from './shopping-bag/shopping-bag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/user.entity/user.entity';
+import { OrderEntity } from './order/order.entity/order.entity';
+import { ProductEntity } from './product/product.entity/product.entity';
+import { ShoppingBagEntity } from './shopping-bag/shopping-bag.entity/shopping-bag.entity';
 
 @Module({
-  imports: [UserModule, OrderModule, ProductModule, ShoppingBagModule],
+  imports: [UserModule, OrderModule, ProductModule, ShoppingBagModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'ecommerce',
+      entities: [UserEntity, OrderEntity, ProductEntity, ShoppingBagEntity],
+      dropSchema: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
