2b5ec058b0b81d89795a7f7ba0e9c58ababa8cef
Germán Rojas <garcejr@gmail.com>
2025-06-07 17:37:44 -0500

    Add ProductModule and ProductService with TypeORM integration for ProductEntity

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './product.entity/product.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
+  providers: [ProductService]
+})
 export class ProductModule {}
