d5382675cde7209ee16b51b679c180dad2308495
Germán Rojas <garcejr@gmail.com>
2025-06-10 14:34:50 -0500

    test: add unit tests for OrderService

--- src/order/order.service.spec.ts
+++ src/order/order.service.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { Repository } from 'typeorm';
+import { OrderEntity } from './order.entity/order.entity';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('OrderService', () => {
   let service: OrderService;
+  let repository: Repository<OrderEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [OrderService],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
+    repository = module.get<Repository<OrderEntity>>(getRepositoryToken(OrderEntity));
   });
 
   it('should be defined', () => {
