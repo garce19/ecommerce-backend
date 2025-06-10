069df0fb3cb33e14dd568926a5adaf9d4bde0adb
Germán Rojas <garcejr@gmail.com>
2025-06-10 14:35:37 -0500

    test: enhance ShoppingBagService tests with repository setup

--- src/shopping-bag/shopping-bag.service.spec.ts
+++ src/shopping-bag/shopping-bag.service.spec.ts
@@ -1,8 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ShoppingBagService } from './shopping-bag.service';
+import { ShoppingBagEntity } from './shopping-bag.entity/shopping-bag.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ShoppingBagService', () => {
   let service: ShoppingBagService;
+  let repository: Repository<ShoppingBagEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,6 +14,7 @@ describe('ShoppingBagService', () => {
     }).compile();
 
     service = module.get<ShoppingBagService>(ShoppingBagService);
+    repository = module.get<Repository<ShoppingBagEntity>>(getRepositoryToken(ShoppingBagEntity));
   });
 
   it('should be defined', () => {
