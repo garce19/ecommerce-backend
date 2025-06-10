09bfc2ab2787aaced9710a2f4974d98209866292
Germán Rojas <garcejr@gmail.com>
2025-06-10 14:34:34 -0500

    refactor: enhance UserService tests with TypeOrm configuration and repository setup

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,15 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { Repository } from 'typeorm';
+import { UserEntity } from './user.entity/user.entity';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [UserService],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
   });
 
   it('should be defined', () => {
