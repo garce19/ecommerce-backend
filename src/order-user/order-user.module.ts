b59639cfa2cdf10275f80d34aede433a3541de68
Germán Rojas <garcejr@gmail.com>
2025-06-17 18:55:38 -0500

    feat: include UserEntity in OrderUserModule imports

--- src/order-user/order-user.module.ts
+++ src/order-user/order-user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderUserService } from './order-user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from '../order/order.entity/order.entity';
+import { UserEntity } from '../user/user.entity/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity])],
-  providers: [OrderUserService]
+  imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity])],
+  providers: [OrderUserService],
 })
 export class OrderUserModule {}
