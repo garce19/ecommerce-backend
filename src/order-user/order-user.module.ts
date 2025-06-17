8b8b9b613b85779bcc792969ef0caf4d0d1c81db
Germán Rojas <garcejr@gmail.com>
2025-06-17 18:23:37 -0500

    feat: integrate TypeOrmModule and OrderEntity into OrderUserModule

--- src/order-user/order-user.module.ts
+++ src/order-user/order-user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrderUserService } from './order-user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderEntity } from '../order/order.entity/order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([OrderEntity])],
   providers: [OrderUserService]
 })
 export class OrderUserModule {}
