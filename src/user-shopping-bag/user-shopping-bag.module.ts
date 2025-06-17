a18ac59fc306b0747af33f877313c82df0f4e2ef
Germán Rojas <garcejr@gmail.com>
2025-06-17 18:56:01 -0500

    feat: add TypeOrmModule and entities to UserShoppingBagModule

--- src/user-shopping-bag/user-shopping-bag.module.ts
+++ src/user-shopping-bag/user-shopping-bag.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserShoppingBagService } from './user-shopping-bag.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
+import { ShoppingBagEntity } from '../shopping-bag/shopping-bag.entity/shopping-bag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, ShoppingBagEntity])],
   providers: [UserShoppingBagService]
 })
 export class UserShoppingBagModule {}
