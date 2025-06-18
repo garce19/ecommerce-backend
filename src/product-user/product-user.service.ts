b3b33b4b7f121125e1bf4e3f4be850fbfe20775b
Germán Rojas <garcejr@gmail.com>
2025-06-17 19:10:17 -0500

    feat: implement ProductUserService with repository injections for Product and User entities

--- src/product-user/product-user.service.ts
+++ src/product-user/product-user.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from '../product/product.entity/product.entity';
+import { Repository } from 'typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
 
 @Injectable()
-export class ProductUserService {}
+export class ProductUserService {
+    constructor(
+        @InjectRepository(ProductEntity)
+        private readonly productRepository: Repository<ProductEntity>,
+
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ){}
+}
