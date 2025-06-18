f20e430a51c80146958ff120344081669a991665
Germán Rojas <garcejr@gmail.com>
2025-06-18 17:29:01 -0500

    feat: integrate BusinessErrorsInterceptor into ShoppingBagController for exception handling

--- src/shopping-bag/shopping-bag.controller.ts
+++ src/shopping-bag/shopping-bag.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { ShoppingBagService } from './shopping-bag.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('shopping-bags')
 export class ShoppingBagController {
     constructor(private readonly shoppingBagService: ShoppingBagService) {}
