bbfb7c53772e53386faa13e5d3a3466d00f8f4bb
Germán Rojas <garcejr@gmail.com>
2025-06-18 17:28:49 -0500

    feat: add BusinessErrorsInterceptor to OrderController for handling exceptions

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { OrderService } from './order.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('orders')
 export class OrderController {
     constructor(private readonly orderService: OrderService){}
