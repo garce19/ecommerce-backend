8c0df46529db64b23536b8a6f7d61ec3e74333b8
Germán Rojas <garcejr@gmail.com>
2025-06-18 17:28:55 -0500

    feat: integrate BusinessErrorsInterceptor into ProductController for exception handling

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('products')
 export class ProductController {
     constructor(private readonly productService: ProductService) {}
