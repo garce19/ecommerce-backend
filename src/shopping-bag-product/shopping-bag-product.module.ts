fe2d98ec676bd3246ca615a6809a83888ea8f635
Germán Rojas <garcejr@gmail.com>
2025-06-17 19:55:25 -0500

    feat: add TypeOrmModule imports and repository injections to ShoppingBagProductService

--- src/shopping-bag-product/shopping-bag-product.module.ts
+++ src/shopping-bag-product/shopping-bag-product.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ShoppingBagProductService } from './shopping-bag-product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShoppingBagEntity } from '../shopping-bag/shopping-bag.entity/shopping-bag.entity';
+import { ProductEntity } from '../product/product.entity/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ShoppingBagEntity, ProductEntity])],
   providers: [ShoppingBagProductService]
 })
 export class ShoppingBagProductModule {}
--- src/shopping-bag-product/shopping-bag-product.service.ts
+++ src/shopping-bag-product/shopping-bag-product.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ShoppingBagEntity } from '../shopping-bag/shopping-bag.entity/shopping-bag.entity';
+import { Repository } from 'typeorm';
+import { ProductEntity } from '../product/product.entity/product.entity';
 
 @Injectable()
-export class ShoppingBagProductService {}
+export class ShoppingBagProductService {
+    constructor(
+        @InjectRepository(ShoppingBagEntity)
+        private readonly shoppingBagRepository: Repository<ShoppingBagEntity>,
+
+        @InjectRepository(ProductEntity)
+        private readonly productRepository: Repository<ProductEntity>,
+    ) {}
+}
