f3e3c3da34e5d8afd6db1b7af5f83512bac85f25
Germán Rojas <garcejr@gmail.com>
2025-06-18 17:20:31 -0500

    feat: add OrderController and integrate it into OrderModule

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './order.entity/order.entity';
+import { OrderController } from './order.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderEntity])],
-  providers: [OrderService]
+  providers: [OrderService],
+  controllers: [OrderController]
 })
 export class OrderModule {}
