42ba71b7cb54a2d76355a1520fccaf14d8f7fe59
Germán Rojas <garcejr@gmail.com>
2025-06-18 17:28:43 -0500

    feat: integrate BusinessErrorsInterceptor into UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
+import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 
+@UseInterceptors(BusinessErrorsInterceptor)
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
