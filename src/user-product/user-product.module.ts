2e80c4ed59dce2416905965519791bc1aff982b9
Germán Rojas <garcejr@gmail.com>
2025-06-17 19:36:36 -0500

    feat: add TypeOrmModule imports and repository injections to UserProductService

--- src/user-product/user-product.module.ts
+++ src/user-product/user-product.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserProductService } from './user-product.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
+import { ProductEntity } from '../product/product.entity/product.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, ProductEntity])], 
   providers: [UserProductService]
 })
 export class UserProductModule {}
--- src/user-product/user-product.service.ts
+++ src/user-product/user-product.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
+import { Repository } from 'typeorm';
+import { ProductEntity } from '../product/product.entity/product.entity';
 
 @Injectable()
-export class UserProductService {}
+export class UserProductService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+
+        @InjectRepository(ProductEntity)
+        private readonly productRepository: Repository<ProductEntity>,
+    ){}
+}
