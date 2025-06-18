94fb24ad589e8fc67b6bcb9d423f5756cba96572
Germán Rojas <garcejr@gmail.com>
2025-06-17 19:20:29 -0500

    feat: add TypeOrmModule imports and repository injections to UserOrderService

--- src/user-order/user-order.module.ts
+++ src/user-order/user-order.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserOrderService } from './user-order.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
+import { OrderEntity } from '../order/order.entity/order.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, OrderEntity])],
   providers: [UserOrderService]
 })
 export class UserOrderModule {}
--- src/user-order/user-order.service.ts
+++ src/user-order/user-order.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../user/user.entity/user.entity';
+import { Repository } from 'typeorm';
+import { OrderEntity } from '../order/order.entity/order.entity';
 
 @Injectable()
-export class UserOrderService {}
+export class UserOrderService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+
+        @InjectRepository(OrderEntity)
+        private readonly orderRepository: Repository<OrderEntity>,
+    ){}
+}
